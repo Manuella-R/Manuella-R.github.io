17c16159e141b178aa813025297a11ec426587e6
Manuella-R <67600619+Manuella-R@users.noreply.github.com>
2024-02-05 16:04:04 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-document.getElementById("downloadButton").addEventListener("click", function() {
-
-    var downloadLink = document.createElement("a");
-    
-    downloadLink.href = "cv/rehema.pdf";
-
-    downloadLink.download = "Rehema_CV.pdf";
-    
-    document.body.appendChild(downloadLink);
-
-    downloadLink.click();
-    
-    document.body.removeChild(downloadLink);
-});
-
 var typed = new Typed(".typing", {
     strings: ["Web developer", "UX/UI designer", "App Developer", "App Tester"],
     typeSpeed: 100,
